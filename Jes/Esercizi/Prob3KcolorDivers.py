26ab6f833373678baadc0716cb0019044ce94444
ArduSan <ferretti2003simone@gmail.com>
2022-11-24 10:12:25 +0100

    Esercizi per Grassi

--- Jes/Esercizi/Prob3KcolorDivers.py
+++ Jes/Esercizi/Prob3KcolorDivers.py
@@ -1,7 +1,10 @@
 
 
 def colorDivers(pic):
+#@param pic:picture
+#@return int
     px=getPixels(pic)
+    count=0
     for i in range (0, len(px)):
         for j in range(0,len(px)):
             colpx=getColor(px[i])
@@ -11,11 +14,18 @@ def colorDivers(pic):
     return count
 
 def almenoK(pic,k):
+#@param pic:picture
+#@param k : int
+#@return bool
     if colorDivers(pic)>=k:
         return True
     return False
 
 def foo(pic1,pic2,k):
+#@param pic1:picture1
+#@param pic2:picture2
+#@param k: int
+#@return bool
     if almenoK(pic1,k) and almenoK(pic2,k):
         return True
     return False
---
+++ Jes/Esercizi/problema8.py
@@ -0,0 +1,56 @@
+
+def lum(p):
+#@def p: pixel
+#return int
+    return (getRed(p)+getGreen(p)+getBlue(p))/3.0
+
+def lumMed(y):
+#@param y: int
+#@return int
+    lumM=0
+    for x in range(0,getWidth(pic)):
+        px=getPixel(pic,x,y)
+        lumM+=lum(px)
+    return lumM/getWidth(pic)
+
+
+def riga(pic):
+#@def pic:picture
+#@return int
+    for y in range(0,getHeight(pic)):
+        if lumMed(y)!=lumMed(min(y+1,getHeight(pic))):
+            return False
+    return True
+
+###########################metodo2, più ostico ma con i quant
+
+def verF(lumA,pic):
+#@param pic:picture
+#@param lumA: int
+#@return bool
+    lumB=0
+    for y in range(0,getHeight(pic)):
+        lumR=0
+        for x in range(0,getWidth(pic));
+            px=getPixel(pic)
+            lumR+=lum(px)
+        lumB=lumR/getWidth(pic)
+        if lumA!=lumB:
+            return False
+    return True
+
+def foo(pic):
+#@param pic: picture
+#@return bool
+    lumA=0
+    for y in range(0, getHeight(pic)):
+        lumR=0
+        for x in range(0,getWidth(pic));
+            px=getPixel(pic)
+            lumR+=lum(px)
+        lumA=lumR/getWidth(pic)
+        if verF(lumA,pic)==False:
+            return False
+    return True
+
+
