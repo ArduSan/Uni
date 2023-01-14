ea7d17a3ee979ac0181c65fcd0466946303ce15e
ArduSan <58180730+ArduSan@users.noreply.github.com>
2023-01-14 15:34:48 +0100

    Add files via upload

---
+++ EserciziPython/python2.0/Matrici/Matrice1.py
@@ -0,0 +1,32 @@
+
+def invertiCR(x,R,C):
+  mat1=matrix(R,C)
+  riga=[]
+  colonna=[]
+  if x==0:
+    return mat1
+  for i in range(0,R):
+    mat1[i]=input("write a row as a list of " + str(C) + " values (enclosed in square brackets): ")
+  riga=mat1[x]
+  
+  for i in range(0,R):
+    colonna.append(mat1[i][x])
+    mat1[i][x]=riga[i]
+  for c in range(0,C):
+    mat1[x][c]=colonna[c]
+  return mat1
+
+def matrix(R,C):
+#@param R: int; number of rows
+#@param C: int; number of columns
+  mat=[]
+  for i in range(0,R):
+    row=[0]*C
+    mat.append(row)
+  return mat
+      
+  
+    
+      
+        
+    
