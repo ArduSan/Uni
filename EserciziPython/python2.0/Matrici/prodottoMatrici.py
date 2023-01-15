0a25697223c925041bea6a453c1baeb6f9410d90
ArduSan <ferretti2003simone@gmail.com>
2023-01-15 16:18:09 +0100

    prodotto matrici

---
+++ EserciziPython/python2.0/Matrici/prodottoMatrici.py
@@ -0,0 +1,21 @@
+def prodottoM(mat1,mat2):
+#@param mat1: matrix
+#@param mat2: matrix
+#@return matrix
+    matP=[]
+    for i in range(0,len(mat1)):
+        for j in range(0,len(mat1[i])):
+            prod=mat1[i][j]*mat2[j][i]
+            matP.append(prod)
+    return matP
+def matrix(numR,numC):
+#@param numR:int;number of rows
+#@param numC:int;number of coloumns
+#@return matrix
+    mat=[]
+    for i in range(0,numR):
+        row=[0]*numC
+        mat.append(row)
+    for j in range(0,numR):
+        mat[j]=input("inserisici "+str(numC)+" valori separati da virgola tra parentesi quadre: ")
+    return mat
