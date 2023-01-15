c7df24ed1b68a8b1582c79f9c5e32c996a8add49
ArduSan <58180730+ArduSan@users.noreply.github.com>
2023-01-15 23:36:51 +0100

    Update prodottoMatrici.py

--- EserciziPython/python2.0/Matrici/prodottoMatrici.py
+++ EserciziPython/python2.0/Matrici/prodottoMatrici.py
@@ -3,14 +3,19 @@ def prodottoM(mat1,mat2):
 #@param mat2: matrix
 #@return matrix
     matP=[]
-    for i in range(0,len(mat1)):
-        for n in range(0,len(mat2)):
-            som=0
-            for j in range(0,len(mat1[i])):
-                prod=mat1[i][j]*mat2[j][n]
-                som+=prod
-        matP.append(som)
+    if controllo(mat1,mat2):
+      for i in range(0,len(mat1)):
+        Ls=[]
+        for n in range(0,len(mat2[0])):
+          som=0
+          for j in range(0,len(mat1[i])):
+            prod=mat1[i][j]*mat2[j][n]
+            som+=prod
+          Ls.append(som)
+        matP.append(Ls)
     return matP
+    
+    
 def matrix(numR,numC):
 #@param numR:int;number of rows
 #@param numC:int;number of coloumns
@@ -22,3 +27,13 @@ def matrix(numR,numC):
     for j in range(0,numR):
         mat[j]=input("inserisici "+str(numC)+" valori separati da virgola tra parentesi quadre: ")
     return mat
+    
+def controllo(mat1,mat2):
+#@param mat1:matrix 
+#@param mat2:matrix
+#return bool
+  for i in range(0,len(mat1)):
+    if len(mat2)==len(mat1[i]):
+      return True
+    else: 
+      return False
