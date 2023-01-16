490d29224b51fddc31095580ee682dc22f30e7c9
ArduSan <58180730+ArduSan@users.noreply.github.com>
2023-01-16 17:59:55 +0100

    Update ricorsivepalindrome.py

--- EserciziPython/python2.0/Ricorsione/ricorsivepalindrome.py
+++ EserciziPython/python2.0/Ricorsione/ricorsivepalindrome.py
@@ -1,11 +1,25 @@
-def nodoubleRicor(str):
-#@param str: string 
-  newStr=""
-  if len(str)==1:
-    return newStr
-  if str[0]!=str[1]:
-    newStr+=str[1:]
-    nodoubleRicor(str[1:])
-  else:
-    nodoubleRicor(str[1:])
-  return newStr
+def palindrome(str):
+#@return str:string
+#@return bool 
+  mid=len(str)//2
+  left=str[:mid]
+  right=str[mid:]
+  if len(str)<2:
+    return True
+
+  if len(str/2)%2==1:
+    Dst=left+str[mid+1:]
+    return palindrome(Dst)
+
+  if len(str)==2:
+    if str[0]==str[1]:
+      return True
+    else:
+      return False
+
+  if str[mid-1]==str[mid]:
+    nSt=str[:mid-2]+str[mid+1:]
+    return palindrome(nSt)
+    
+  if str[mid-1]!=str[mid]:
+    return False
