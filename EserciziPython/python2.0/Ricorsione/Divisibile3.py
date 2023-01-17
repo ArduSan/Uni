705f9d4a2e870c4e6b9d52dfd9a053d5feb0b326
ArduSan <ferretti2003simone@gmail.com>
2023-01-17 18:25:51 +0100

    esercizi grassi

--- EserciziPython/python2.0/Ricorsione/Divisibile3.py
+++ EserciziPython/python2.0/Ricorsione/Divisibile3.py
@@ -1,6 +1,8 @@
 def divisibile3(n):
-    if n<10:
-        return n%3==0
+    if n<3 or (n<9 and n>6) or (n>3 and n<6):
+        return False
+    if n==3 or n==9 or n==6:
+        return True
     else:
         return divisibile3(sum(map(int,str(n))))
 
