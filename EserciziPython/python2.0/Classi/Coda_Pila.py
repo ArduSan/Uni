26e6404856c298f6fe2c5c97c9f02643c29842a5
ArduSan <ferretti2003simone@gmail.com>
2023-01-18 20:36:48 +0100

    esercizi foi

--- EserciziPython/python2.0/Classi/Coda_Pila.py
+++ EserciziPython/python2.0/Classi/Coda_Pila.py
@@ -14,4 +14,20 @@ class Stack:
         else:
             return False
     
-
+class Queue:
+    def __init__(self):
+        self.List2=[]
+    def in(self,elem):
+        self.List2.append(elem)
+    def out(self):
+        if len(self.List2)>0:
+            self.List2.pop(0)
+    def first(self):
+        print self.List2[0]
+    def last(self):
+        print self.List2[len(self.List2)-1]
+    def empty(self):
+        if self.List==[]:
+            return True
+        else:
+            return False
