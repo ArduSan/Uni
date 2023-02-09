2338f31fa8a50b6b0b5db572d5a477ed533ab8cd
ArduSan <58180730+ArduSan@users.noreply.github.com>
2023-02-09 16:47:26 +0100

    Update Matrice1.py

--- EserciziPython/python2.0/Matrici/Matrice1.py
+++ EserciziPython/python2.0/Matrici/Matrice1.py
@@ -1,33 +1,26 @@
-
-def invertiCR(x,R,C):
-#@param x:int
-#@param R:int; number of rows
-#@param C:int; number of columns
-  mat1=matrix(R,C)
-  riga=[]
-  colonna=[]
-  if x==0:
-    return mat1
+def invertiCR(R,x):
+#@param R,x:int; number of colums,rows
+#@return mat
+  mat=matrix(R)
+  col=[]
+  rig=mat[x]
   for i in range(0,R):
-    mat1[i]=input("write a row as a list of " + str(C) + " values (enclosed in square brackets): ")
-  riga=mat1[x]
+    col+=[mat[i][x]]
+  mat[x]=col
+  for j in range(0,R):
+    mat[j][x]=rig[j]
+  return mat
   
-  for i in range(0,R):
-    colonna.append(mat1[i][x])
-    mat1[i][x]=riga[i]
-  for c in range(0,C):
-    mat1[x][c]=colonna[c]
-  return mat1
-
-def matrix(R,C):
-#@param R: int; number of rows
-#@param C: int; number of columns
+def matrix(R):
+#@param R:int 
+#@return mat
   mat=[]
   for i in range(0,R):
-    row=[0]*C
-    mat.append(row)
-  return mat
-      
+    row=[0]*R
+    mat.append(R)
+  for j in range(0,R):
+    mat[j]=input("inserisci "+str(R)+" valori tra parentesi quadrate: ")
+  return mat 
   
     
       
