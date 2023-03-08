404033a1dc031b6109811a8514b42193bcbe0360
ArduSan <58180730+ArduSan@users.noreply.github.com>
2023-03-08 11:56:28 +0100

    Delete Jes/Esercizi directory

--- Jes/Esercizi/AsteriscoperAlphanume.py
+++
@@ -1,9 +0,0 @@
-def AlfaNum(s):
-#@param s: string 
-#@return string 
-  newStr=""
-  count=0
-  for i in range(0, len(s)):
-    if s[i].isalpha():
-      newStr+="*"
-  return newStr
--- Jes/Esercizi/inserisci.py
+++
@@ -1,9 +0,0 @@
-def inserisci(L,n1,n2):
-    newL=[]
-    for i in range(0,len(L)):
-        if L[i]==n1:
-            newL.append(n1)
-            newL.append(n2)
-        else:
-            newL.append(L[i])
-    return newL
